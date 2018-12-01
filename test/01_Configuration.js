e38943e1d23b327cf2aa27e828de707d22aaf16b
Eray Hanoğlu <erayszn@gmx.net>
2018-12-01 14:43:43 +0300

    [+] Added array support [-] Dropped multiple format definition support

--- test/01_Configuration.js
+++ test/01_Configuration.js
@@ -7,15 +7,9 @@ const path = require('path');
 describe('Configuration', function() {
 
   const schema1 = {
-    env: {
-      doc: 'The application environment.',
-      format: ['production', 'development', 'test'],
-      default: 'development',
-      env: 'NODE_ENV'
-    },
     ip: {
       doc: 'The IP address to bind.',
-      format: 'ip|url',
+      format: 'url',
       default: '127.0.0.1',
       env: 'IP_ADDRESS'
     },
@@ -30,8 +24,7 @@ describe('Configuration', function() {
       properties: {
         arg1: {
           doc: 'Sub arg-1',
-          format: 'int',
-          arg: 'testarg1'
+          format: 'int'
         },
         arg2: {
           doc: 'Sub arg-2',
@@ -42,7 +35,7 @@ describe('Configuration', function() {
           default: '1 sec'
         },
         arg4: {
-          format: ['aa', 'bb', 'cc']
+          enum: ['aa', 'bb', 'cc']
         }
       }
     }
@@ -54,32 +47,18 @@ describe('Configuration', function() {
     config = new Configuration();
   });
 
-  it('should add child node', function() {
-    config.addChild('a');
-    assert.equal(config.children.a.parent, config);
-    assert.equal(config.children.a.root, config);
-  });
-
-  it('should get node path', function() {
-    const a = config.addChild('a');
-    const b = a.addChild('b');
-    assert.equal(config.path, '');
-    assert.equal(a.path, 'a');
-    assert.equal(b.path, 'a.b');
-  });
-
-  it('should define property', function() {
+  it('should define property- define(key:string, def:object)', function() {
     config.define('prop1', {
       format: 'string'
     });
-    assert.deepEqual(config._schema, {
+    assert.deepStrictEqual(config._schema, {
       prop1: {format: 'string'}
     });
   });
 
-  it('should define property - string argument', function() {
+  it('should define property - define(key:string, format:string)', function() {
     config.define('prop1', 'string');
-    assert.deepEqual(config._schema, {
+    assert.deepStrictEqual(config._schema, {
       prop1: {format: 'string'}
     });
   });
@@ -94,23 +73,26 @@ describe('Configuration', function() {
     assert.throws(() => {
       config.define('a', 123);
     }, /You must provide./g);
-  });
-
-  it('should define() check "format" property exists and not empty', function() {
     assert.throws(() => {
       config.define('p1', {});
-    }, /You must provide./g);
+    }, /You must provide/g);
   });
 
   it('should define() check "format" exists', function() {
     assert.throws(() => {
-      config.define('p1', {format: 'notexists'});
-    }, /No such format./g);
+      config.define('p1', 'notexists');
+    }, /Unknown format./g);
   });
 
-  it('should defineAll() define properties at once', function() {
+  it('should define() check "format" is string', function() {
+    assert.throws(() => {
+      config.define('p1', {format: 123});
+    }, /must be string/g);
+  });
+
+  it('should define multiple properties at once - defineAll()', function() {
     config.defineAll(schema1);
-    assert.deepEqual(config.getSchema(), schema1);
+    assert.deepStrictEqual(config.getSchema(), schema1);
   });
 
   it('should defineAll() validate argument type', function() {
@@ -119,11 +101,95 @@ describe('Configuration', function() {
     }, /You must provide./g);
   });
 
-  it('should has() check if property exists', function() {
+  it('should add child node', function() {
+    config.addChild('a');
+    assert.strictEqual(config.children.a.parent, config);
+    assert.strictEqual(config.children.a.root, config);
+  });
+
+  it('should get node path', function() {
+    const a = config.addChild('a');
+    const b = a.addChild('b');
+    assert.strictEqual(config.path, '');
+    assert.strictEqual(a.path, 'a');
+    assert.strictEqual(b.path, 'a.b');
+  });
+
+  it('should check if property exists - has()', function() {
     config.defineAll(schema1);
-    assert.equal(config.has('ip'), true);
-    assert.equal(config.has('sub.arg1'), true);
-    assert.equal(config.has('sub.nonexist'), false);
+    assert.strictEqual(config.has('ip'), true);
+    assert.strictEqual(config.has('sub.arg1'), true);
+    assert.strictEqual(config.has('sub.nonexist'), false);
+  });
+
+  it('should get() return value assigned using set()', function() {
+    const o = config.define('prop1', {
+      format: 'number',
+      default: 80
+    });
+    o.set('prop1', 234);
+    assert.strictEqual(o.get('prop1'), 234);
+  });
+
+  it('should get() return original value', function() {
+    const o = config.define('prop1', {
+      format: 'duration'
+    });
+    o.set('prop1', '1 sec');
+    assert.strictEqual(o.get('prop1'), 1000);
+    assert.strictEqual(o.get('prop1', true), '1 sec');
+  });
+
+  it('should get() return loaded value (if value not set)', function() {
+    const o = config.define('prop1', {
+      format: 'number',
+      default: 80
+    });
+    config.load({prop1: 8081});
+    assert.strictEqual(o.get('prop1'), 8081);
+  });
+
+  it('should get() return env value (if value not set)', function() {
+    const o = config.define('prop1', {
+      enum: ['production', 'development', 'test'],
+      default: 'development',
+      env: 'NODE_ENV'
+    });
+    assert.strictEqual(o.get('prop1'), 'test');
+    o.set('prop1', 'production');
+    assert.strictEqual(o.get('prop1'), 'production');
+  });
+
+  it('should get() return arg value (if value not set)', function() {
+    const o = config.define('prop1', {
+      format: 'number',
+      arg: 'testarg1',
+      default: 80
+    });
+    assert.strictEqual(o.get('prop1'), 8081);
+  });
+
+  it('should get() return default value (if value not set)', function() {
+    const o = config.define('prop1', {
+      format: 'number',
+      default: 80
+    });
+    assert.strictEqual(o.get('prop1'), 80);
+  });
+
+  it('should get() return original default value', function() {
+    const o = config.define('prop1', {
+      format: 'duration',
+      default: '1 sec'
+    });
+    assert.strictEqual(o.get('prop1'), 1000);
+    assert.strictEqual(o.get('prop1', true), '1 sec');
+  });
+
+  it('should get() throw if property is not defined', function() {
+    assert.throws(() => {
+      config.get('unknown');
+    }, /.*is not defined.*/g);
   });
 
   it('should get() validate first argument is string', function() {
@@ -133,73 +199,43 @@ describe('Configuration', function() {
     }, /You must provide.*/);
   });
 
-  it('should return env value if value not set', function() {
-    const o = config
-        .defineAll(schema1);
-    assert.equal(o.get('env'), 'test');
-    o.set('env', 'production');
-    assert.equal(o.get('env'), 'production');
-  });
-
-  it('should return arg value if value not set', function() {
-    const o = config
-        .defineAll(schema1);
-    assert.equal(o.get('sub.arg1'), 8081);
-    assert.equal(o.get('sub.arg1', true), '8081');
-    o.set('sub.arg1', '83');
-    assert.equal(o.get('sub.arg1'), 83);
-    assert.equal(o.get('sub.arg1', true), '83');
-  });
-
-  it('should return loaded value if value not set', function() {
-    const o = config
-        .defineAll(schema1)
-        .load({sub: {arg3: 125, arg4: 'aa'}});
-    assert.equal(o.get('sub.arg3'), 125);
-    o.set('sub.arg3', 234);
-    assert.equal(o.get('sub.arg3'), 234);
-    assert.equal(o.get('sub.arg4'), 'aa');
-  });
-
-  it('should return default value if value not set', function() {
-    config.defineAll(schema1);
-    assert.equal(config.get('sub.arg3'), 1000);
-    assert.equal(config.get('sub.arg3', true), '1 sec');
-  });
-
-  it('should get() return assigned value by set()', function() {
-    config.defineAll(schema1);
-    config.set('ip', '127.0.0.2');
-    config.set('sub.arg3', '234');
-    assert.equal(config.get('ip'), '127.0.0.2');
-    assert.equal(config.get('sub.arg3'), 234);
-  });
-
-  it('should get() throw if property is not defined', function() {
-    assert.throws(() => {
-      config.defineAll(schema1);
-      config.get('unknown');
-    }, /.*is not defined.*/g);
-  });
-
   it('should set() throw if property is not defined', function() {
     assert.throws(() => {
-      config.defineAll(schema1);
       config.set('unknown', 1);
     }, /.*is not defined.*/g);
   });
 
-  it('should set() accept enum values if format is an array', function() {
-    config.defineAll(schema1);
-    config.set('sub.arg4', 'bb');
+  it('should set enum values', function() {
+    config.define('enum1', [1, 2, 3]);
+    config.define('enum2', {enum: 'a'});
+    config.define('enum3', {enum: 'a'});
+    config.set('enum1', 1);
+    config.set('enum2', 'a');
     assert.throws(() => {
-      config.set('sub.arg4', 'abc');
+      config.set('enum1', 'e');
     }, /Validation error.*/g);
+    assert.throws(() => {
+      config.set('enum2', 'b');
+    }, /Validation error.*/g);
+    assert.strictEqual(config.get('enum1'), 1);
+    assert.strictEqual(config.get('enum2'), 'a');
+  });
+
+  it('should set array values', function() {
+    config.define('array1', '[string]');
+    config.define('array2', '[int]');
+    config.set('array1', [1, 2, 3]);
+    config.set('array2', 1);
+    assert.throws(() => {
+      config.set('array1', {});
+    }, /Validation error.*/g);
+    assert.deepStrictEqual(config.get('array1'), ['1', '2', '3']);
+    assert.deepStrictEqual(config.get('array2'), [1]);
   });
 
   it('should set() throw if property is not valid', function() {
     assert.throws(() => {
-      config.defineAll(schema1);
+      config.define('port', 'port');
       config.set('port', -1);
     }, /Validation error.*/g);
   });
@@ -224,12 +260,12 @@ describe('Configuration', function() {
     config.defineAll(schema1);
     config.set('ip', '127.0.0.2');
     config.set('sub.arg3', '234');
-    assert.equal(config.get('ip'), '127.0.0.2');
-    assert.equal(config.get('sub.arg3'), 234);
+    assert.strictEqual(config.get('ip'), '127.0.0.2');
+    assert.strictEqual(config.get('sub.arg3'), 234);
     config.reset('ip');
     config.reset('sub.arg3');
-    assert.equal(config.get('ip'), '127.0.0.1');
-    assert.equal(config.get('sub.arg3'), 1000);
+    assert.strictEqual(config.get('ip'), '127.0.0.1');
+    assert.strictEqual(config.get('sub.arg3'), 1000);
   });
 
   it('should get all values as json object', function() {
@@ -237,12 +273,10 @@ describe('Configuration', function() {
     config.set('ip', '127.0.0.2');
     config.set('sub.arg3', '234');
     let o = config.toJSON();
-    assert.deepEqual(o, {
-      env: 'test',
+    assert.deepStrictEqual(o, {
       ip: '127.0.0.2',
       port: 8080,
       sub: {
-        arg1: 8081,
         arg3: 234
       }
     });
@@ -279,24 +313,29 @@ describe('Configuration', function() {
   it('should addFormat() validate first argument', function() {
     assert.throws(() => {
       config.addFormat(123);
-    }, /You must provide Object instance.*/);
+    }, /You must provide/);
+  });
+
+  it('should addFormat() validate second argument', function() {
+    assert.throws(() => {
+      config.addFormat('prop1');
+    }, /You must provide/);
   });
 
   it('should addFormat() validate "name" property defined', function() {
     assert.throws(() => {
       config.addFormat({});
-    }, /You must provide "name" property.*/);
+    }, /You must provide string value.*/);
   });
 
   it('should add custom format', function() {
-    config.addFormat({
-      name: 'country-code',
+    config.addFormat('country-code', {
       parse: (v) => String(v),
       validate: (v) => v && v.length === 2
     });
     config.define('country', 'country-code');
     config.set('country', 'TR');
-    assert.equal(config.get('country'), 'TR');
+    assert.strictEqual(config.get('country'), 'TR');
     assert.throws(() => {
       config.set('country', 'TRY');
     }, /Validation error.*/);
@@ -310,12 +349,10 @@ describe('Configuration', function() {
           sub: {arg3: 234}
         })
         .toJSON();
-    assert.deepEqual(o, {
-      env: 'test', // read from env
+    assert.deepStrictEqual(o, {
       ip: '127.0.0.2',
       port: 8080,
       sub: {
-        arg1: 8081, // read from args
         arg3: 234
       }
     });
@@ -326,12 +363,10 @@ describe('Configuration', function() {
         .defineAll(schema1)
         .loadFiles(path.join(__dirname, 'support', 'val1.json'))
         .toJSON();
-    assert.deepEqual(o, {
-      env: 'test',
+    assert.deepStrictEqual(o, {
       ip: '127.0.0.2',
       port: 8080,
       sub: {
-        arg1: 8081,
         arg3: 234
       }
     });
@@ -343,12 +378,10 @@ describe('Configuration', function() {
         .addParsers({jsonx: [() => null, JSON.parse]})
         .loadFiles([path.join(__dirname, 'support', 'val1.jsonx')])
         .toJSON();
-    assert.deepEqual(o, {
-      env: 'test',
+    assert.deepStrictEqual(o, {
       ip: '127.0.0.2',
       port: 8080,
       sub: {
-        arg1: 8081,
         arg3: 234
       }
     });
